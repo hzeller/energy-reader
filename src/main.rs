59f943c4d13ec0da86a92309fe9e7af45cf04def
Henner Zeller <h.zeller@acm.org>
2025-12-30 20:51:10 +0100

    Put digit locate in separate function; remove debugging graphing.

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,18 @@
-#![allow(dead_code)]
-#![allow(unreachable_code)]
-
 use image::{DynamicImage,GrayImage,Luma};
 use std::env;
 use std::cmp;
 
 const THRESHOLD:f32 = 0.6;
-//const THRESHOLD:f32 = 0.4;  // w/o sobel
 
-fn load_as_grayscale(path : &str) -> GrayImage {
+fn load_image_as_grayscale(path : &str) -> GrayImage {
     let rgba = image::open(path).unwrap().into_rgba8();
     DynamicImage::ImageRgba8(rgba).into_luma8()
 }
 
+// Score of the particular digit image at a particular position.
 type ColumnFeatureScore = Vec<f32>;
 
-fn graph(values: &ColumnFeatureScore, height: u32) -> GrayImage {
-    let mut result = GrayImage::new(values.len() as u32, height);
-    let white = Luma::<u8>::from([255;1]);
-
-    for ix in 0..values.len() {
-	let value = values[ix];
-	let img_range = (height - 1) as f32;
-	let iy = ((1.0 - value) * img_range) as u32;
-	result.put_pixel(ix as u32, iy, white);
-	if value > THRESHOLD {
-	    for y in iy..height {
-		result.put_pixel(ix as u32, y, white);
-	    }
-	}
-    }
-    result
-}
-
+// Determine score of given needle pattern existing at given haystack column.
 fn cross_correlate(haystack: &GrayImage, needle: &GrayImage)
 		   -> ColumnFeatureScore {
     // Prepare to normalize the output.
@@ -58,123 +38,101 @@ fn cross_correlate(haystack: &GrayImage, needle: &GrayImage)
     output
 }
 
+// Classic edge detection.
 fn sobel(input: &GrayImage) -> GrayImage {
-    //return input.clone();
-
     let width: u32 = input.width() - 2;
     let height: u32 = input.height() - 2;
-    let mut buff = GrayImage::new(width, height);
+    let mut result = GrayImage::new(width, height);
 
     for x in 0..width {
         for y in 0..height {
             let nw = input.get_pixel(x, y)[0] as i32;
+	    let north = input.get_pixel(x + 1 , y)[0] as i32;
             let ne = input.get_pixel(x + 2, y)[0] as i32;
 
-            let north = input.get_pixel(x + 1 , y)[0] as i32;
             let west = input.get_pixel(x, y + 1)[0] as i32;
             let east = input.get_pixel(x + 2, y + 1)[0] as i32;
-            let south = input.get_pixel(x + 1, y + 2)[0] as i32;
 
 	    let sw = input.get_pixel(x, y + 2)[0] as i32;
+	    let south = input.get_pixel(x + 1, y + 2)[0] as i32;
             let se = input.get_pixel(x + 2, y + 2)[0] as i32;
 
-	    let gx = (-1 *   nw) + (1 *   ne)
+	    // Sobel kernel in x and y direction
+	    let gx = -nw + ne
 		+    (-2 * west) + (2 * east)
-		+    (-1 *   sw) + (1 *   se);
-	    let gy = (-1 * nw) + (-2 * north) + (-1 * ne)
-                +    ( 1 * sw) + ( 2 * south) + ( 1 * se);
+		+    -sw + se;
+	    let gy = -nw + (-2 * north) + -ne
+                +     sw + ( 2 * south) + se;
 
             let mut mag = ((gx as f64).powi(2) + (gy as f64).powi(2)).sqrt();
 
-            if mag > 255.0 {
-                mag = 255.0;
-            }
+	    mag = mag.clamp(0.0, 255.0);
 
-            buff.put_pixel(x, y, Luma([mag as u8]));
+            result.put_pixel(x, y, Luma([mag as u8]));
         }
     }
-    return buff;
+    result
+}
+
+struct DigitPos {
+    digit: u32,
+    score: f32,
+    pos: u32,
+}
+
+fn locate_digits(scores: &[ColumnFeatureScore], picture_width: u32,
+		 digit_width: u32)
+		 -> Vec<DigitPos> {
+    let mut result = Vec::new();
+    let mut current = DigitPos{digit: u32::MAX, score: 0.0, pos: picture_width};
+    for x in 0..picture_width {
+	// What is the highest scoring digit in each of the picture columns
+	for (i, feature_score) in scores.iter().enumerate() {
+	    let digit_score = feature_score[x as usize];
+	    if digit_score < THRESHOLD {
+		continue;
+	    }
+	    if digit_score > current.score {
+		current.digit = i as u32;
+		current.score = digit_score;
+		current.pos = x;
+	    }
+	}
+
+	if x >= current.pos + digit_width {  // best seen for digit-width
+	    result.push(current);
+	    current = DigitPos{digit: u32::MAX, score: 0.0, pos: picture_width};
+	}
+    }
+    result
 }
 
 // Params: energy-reader image <digit0> <digit1>...
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     // First image is the text containing image, followed by digits.
-    let haystack_raw = load_as_grayscale(env::args().nth(1).unwrap().as_str());
-    let haystack = sobel(&haystack_raw);
+    let haystack_file = env::args().nth(1).unwrap();
+    let haystack = sobel(&load_image_as_grayscale(haystack_file.as_str()));
 
     let mut max_digit_width = 0;
     let mut digits = Vec::new();
-    for picture in env::args().skip(2) {
-	let digit = sobel(&load_as_grayscale(picture.as_str()));
+    for digit_picture in env::args().skip(2) {
+	let digit = sobel(&load_image_as_grayscale(digit_picture.as_str()));
 	max_digit_width = cmp::max(max_digit_width, digit.width());
 	digits.push(digit);
     }
 
-    let mut vertical_pos = 0;
-    let mut output = GrayImage::new(max_digit_width + haystack.width(),
-				    (digits.len() as u32+2) * haystack.height());
-    // Original as first
-    image::imageops::overlay(&mut output, &haystack, max_digit_width as i64,
-			     vertical_pos as i64);
-    vertical_pos += haystack.height();
-
-    // the heatmap/graphs of digit matches
-    let mut digit_scores : Vec<Vec<f32>> = Vec::new();
+    // Create similarity score per haystack pixel column.
+    let mut digit_scores : Vec<ColumnFeatureScore> = Vec::new();
     for digit in digits.iter() {
 	let highest_column = cross_correlate(&haystack, digit);
 	digit_scores.push(highest_column);
-	let visualize = graph(&digit_scores[digit_scores.len()-1],
-			      haystack.height());
-
-	image::imageops::overlay(&mut output, digit, 0,
-				 vertical_pos as i64);
-	image::imageops::overlay(&mut output, &visualize, max_digit_width as i64,
-				 vertical_pos as i64);
-
-	vertical_pos += haystack.height();
     }
 
-    let mut last_print_pos = 0;
-    let mut decision_cutoff = u32::MAX;
-    let mut decision_score = 0.0;
-    let mut decision_index = -1;
-
-    for x in 0..haystack.width() {
-	// What is the highest scoring digit
-	let mut found_index = -1;
-	for i in 0..digit_scores.len() {
-	    let feature_score = &digit_scores[i];
-	    let value = feature_score[x as usize];
-	    if value < THRESHOLD {
-		continue;
-	    }
-	    if value > decision_score {
-		found_index = i as i32;
-		decision_score = value;
-	    }
-	}
-
-	if found_index >= 0 && found_index != decision_index {
-	    decision_index = found_index;
-	    decision_cutoff = x + digits[decision_index as usize].width() as u32;
-	}
-
-	if x >= decision_cutoff {
-	    println!("Digit {} at {} (Δ={})", decision_index, x,
-		     x - last_print_pos);
-	    last_print_pos = x;
-	    let digit = &digits[decision_index as usize];
-	    image::imageops::overlay(&mut output, digit,
-				     (max_digit_width + (x - digit.width()))
-				     as i64,
-				     vertical_pos as i64);
-	    decision_cutoff = u32::MAX;
-	    decision_index = -1;
-	    decision_score = 0.0;
-	}
+    let digit_locations = locate_digits(&digit_scores, haystack.width(),
+					max_digit_width);
+    for loc in digit_locations {
+	println!("{} {} {:4} {:.3}", loc.digit,
+		 env::args().nth((loc.digit + 2) as usize)
+		 .expect("should be valid arg"), loc.pos, loc.score);
     }
-    // The final chosen match.
-    output.save("output.png")?;
-
-    Ok(())
 }
